b449f7e6b02f777b2fd6a7dab4c74fc3b5baa280
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-07-05 17:44:02 +0530

    Create PaginationComponent.jsx

---
+++ frontend/src/components/PaginationComponent.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
+import ProductCard from './ProductCard';
+
+const PaginationComponent = ({ data }) => {
+  const itemsPerPage = 8; // Number of items to display per page
+  const initialPage = 0; // Initial page number
+  const [currentPage, setCurrentPage] = useState(initialPage);
+  const [pageCount, setPageCount] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const totalPageCount = Math.ceil(data.length / itemsPerPage);
+      setPageCount(totalPageCount);
+    };
+
+    fetchData();
+  }, [data]);
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const offset = currentPage * itemsPerPage;
+  const currentPageData = data.slice(offset, offset + itemsPerPage);
+
+  return (
+    <>
+      {/* Render your data here */}
+      <div className='shopping-items'>
+        {currentPageData.map((item) => (
+          <ProductCard key={item._id} product={item} />
+        ))}
+      </div>
+
+      {/* Render the pagination component */}
+      <div className="pagination-container">
+        <ReactPaginate
+          previousLabel={'Previous'}
+          nextLabel={'Next'}
+          breakLabel={'...'}
+          pageCount={pageCount}
+          marginPagesDisplayed={4}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageChange}
+          containerClassName={'pagination'}
+          activeClassName={'active'}
+        />
+      </div>
+    </>
+  );
+};
+
+export default PaginationComponent;
