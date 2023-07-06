6339fa172ccd588c3823bb8b163097c48bb13a82
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-07-06 15:13:29 +0530

    Update PaytmPaymentGateway.jsx

--- frontend/src/components/PaytmPaymentGateway.jsx
+++ frontend/src/components/PaytmPaymentGateway.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import savePaymentInfo from '../reducers/cartReducers';
+import { savePaymentInfo } from '../reducers/cartReducers';
 import Message from './Message';
 
 const PaytmPaymentGateway = () => {
