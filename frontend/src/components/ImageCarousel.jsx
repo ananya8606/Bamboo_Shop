ec8bcf270b375582a6fc957c879d330bbc9ddc49
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-07-06 19:06:58 +0530

    Update ImageCarousel.jsx

--- frontend/src/components/ImageCarousel.jsx
+++ frontend/src/components/ImageCarousel.jsx
@@ -1,32 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import { Slide } from 'react-slideshow-image'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react';
+import { Slide } from 'react-slideshow-image';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Loading from './Loading';
+import 'react-slideshow-image/dist/styles.css';
 
-import { Link } from 'react-router-dom'
-import Loading from './Loading'
-import 'react-slideshow-image/dist/styles.css'
 const ImageCarousel = () => {
-  const productList = useSelector((state) => state.product.productList)
-  const { loading, products, error } = productList
+  const productList = useSelector((state) => state.product.productList);
+  const { loading, products, error } = productList;
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    // Check if all images are loaded
+    const handleImageLoad = () => {
+      setImagesLoaded(true);
+    };
+
+    const images = Array.from(document.querySelectorAll('.each-slide img'));
+    let loadedCount = 0;
+
+    images.forEach((image) => {
+      if (image.complete) {
+        loadedCount++;
+      } else {
+        image.addEventListener('load', handleImageLoad);
+      }
+    });
+
+    if (loadedCount === images.length) {
+      setImagesLoaded(true);
+    }
+
+    // Clean up event listeners
+    return () => {
+      images.forEach((image) => {
+        image.removeEventListener('load', handleImageLoad);
+      });
+    };
+  }, []);
 
   return (
     <div className='slide-container'>
       {loading && <Loading />}
-      <Slide className='manageSlide'>
-        {products &&
-          products.map((items) => (
-            <div className='each-slide' key={items._id}>
-              <Link
-                to={`/category/${items.category}/${items.subCategory}/${items._id}`}
-              >
-                {' '}
-                <img src={items.image} alt='' />
-              </Link>
-            </div>
-          ))}
-      </Slide>
+      {imagesLoaded && (
+        <Slide className='manageSlide'>
+          {products &&
+            products.map((item) => (
+              <div className='each-slide' key={item._id}>
+                <Link
+                  to={`/category/${item.category}/${item.subCategory}/${item._id}`}
+                >
+                  <img src={item.image} alt='' />
+                </Link>
+              </div>
+            ))}
+        </Slide>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ImageCarousel
+export default ImageCarousel;
