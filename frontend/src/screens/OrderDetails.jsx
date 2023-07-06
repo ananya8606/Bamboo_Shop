792b36ceeb937acb71b9c9be3fb98818415f580d
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-07-06 20:57:29 +0530

    Update OrderDetails.jsx

--- frontend/src/screens/OrderDetails.jsx
+++ frontend/src/screens/OrderDetails.jsx
@@ -63,7 +63,7 @@ const OrderDetails = () => {
                 <div className='orderscreen-controller'>
                   <span>{s.paymentmethod[language]}</span>
                   <span className='gd'>{orderdetails.paymentMethod}</span>
-                  { orderdetails.paymentInfo.cardholderName && (
+                  { orderdetails.paymentInfo && (
                  <>
                    <span className='gd'>{s.chn[language]}:{orderdetails.paymentInfo.cardholderName} </span>
                   <span className='gd'>{s.cn[language]}:{orderdetails.paymentInfo.cardNumber} </span>
