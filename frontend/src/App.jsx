3c3f89e7463d998d615a7a76da37ad1561229086
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-07-13 23:11:27 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,12 @@ import Shipping from './screens/Shipping';
 import Payment from './screens/Payment';
 import OrderScreen from './screens/OrderScreen';
 import MyOrders from './screens/MyOrders';
+import MyQueries from './screens/MyQueries';
 import AdminOrders from './screens/AdminOrders';
 import OrderDetails from './screens/OrderDetails';
 import AdminProducts from './screens/AdminProducts';
 import AdminUsers from './screens/AdminUsers';
+import AdminQueries from './screens/AdminQueries';
 import ProductCreate from './screens/ProductCreate';
 import ProductEdit from './screens/ProductEdit';
 import UserAccountScreen from './screens/UserAccountScreen';
@@ -23,6 +25,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import PaytmPaymentGateway from './components/PaytmPaymentGateway';
 import ProductBulk from './screens/ProductBulk';
+import LocationPage from './components/Location';
 
 function App() {
   return (
@@ -41,7 +44,9 @@ function App() {
         <Route path="/payment" element={<Payment />} />
         <Route path="/placeorder" element={<OrderScreen />} />
         <Route path="/myorders/:id" element={<MyOrders />} />
+        <Route path="/myqueries/:id" element={<MyQueries />} />
         <Route path="/admin/allOrders" element={<AdminOrders />} exact />
+        <Route path="/admin/allQueries" element={<AdminQueries />} exact />
         <Route path="/orderDetails/:id" element={<OrderDetails />} exact />
         <Route path="/admin/allProducts" element={<AdminProducts />} exact />
         <Route path="/admin/allUsers" element={<AdminUsers />} exact />
@@ -50,6 +55,7 @@ function App() {
         <Route path="/my-account/:id" element={<UserAccountScreen />} exact />
         <Route path="/paytm-payment-gateway" element={<PaytmPaymentGateway />} exact />
         <Route path="/admin/ProductBulk" element={<ProductBulk />} exact />
+        <Route path="/contact-us" element={<LocationPage />} exact />
       </Routes>
       <Footer />
     </Router>
