1a32a11701076b3b29430ef006a75574471971c9
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-07-13 23:34:11 +0530

    Add files via upload

---
+++ backend/models/Query.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const querySchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+      email:{type:String},
+      type:{type:String},
+      query:{type:String},
+      active:{type:Boolean, default: false,},
+      closed:{type:Boolean, default: false}
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Query = mongoose.model('Query', querySchema)
+module.exports = Query
