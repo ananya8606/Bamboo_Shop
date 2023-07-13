15338b26386d9c3487f746954852e2178cd73343
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-07-13 23:21:41 +0530

    Add files via upload

---
+++ frontend/src/screens/AdminQueries.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loading from '../components/Loading';
+import { listQueries,changeQueryStatus } from '../reducers/userReducers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { aq } from '../Utils/translateLibrary/adminqueries';
+
+const AdminQueries = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.user.userLogin);
+  const { userInformation: userInfo } = userLogin;
+  const queryList = useSelector((state) => state.user.queryList);
+  const { loading, queries, error } = queryList;
+  const settings = useSelector((state) => state.settings);
+  const { language, currency } = settings;
+  const history = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo) {
+      history('/');
+    } else {
+      dispatch(listQueries());
+    }
+  }, [userInfo, history, dispatch]);
+
+  const queryHandler = (id) => {
+    dispatch(changeQueryStatus(id));
+  };
+
+  return (
+    <div className='adminOrdersouter'>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className='adminOrder-inner'>
+          {loading && <Loading />}
+          {queries && queries.length > 0 && (
+            <table>
+              <thead>
+                <tr>
+                  <th>{aq.u[language]}</th>
+                  <th>{aq.ue[language]}</th>
+                  <th>{aq.uq[language]}</th>
+                  <th>{aq.us[language]}</th>
+                  <th>{aq.ac[language]}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {queries.map((query) => (
+                  <tr key={query._id}>
+                    <td>{query.user}</td>
+                    <td>{query.email}</td>
+                    <td>{query.type}</td>
+                    <td>{query.query}</td>
+                    <td
+                      style={{ textAlign: 'center' }}
+                      onClick={() => queryHandler(query._id)}
+                    >
+                      {query.active ? (
+                        <i
+                          className='fas fa-check'
+                          style={{
+                            color: 'green',
+                            cursor: 'pointer',
+                            fontSize: '20px',
+                          }}
+                        ></i>
+                      ) : (
+                        <FontAwesomeIcon
+                          icon={faTimes}
+                          style={{ color: 'red', cursor: 'pointer' }}
+                        />
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AdminQueries;
---
+++ frontend/src/screens/MyQueries.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserDetails } from '../reducers/userReducers'
+import Message from '../components/Message'
+import Loading from '../components/Loading'
+import { useParams,useNavigate } from 'react-router-dom';
+import { mq } from '../Utils/translateLibrary/myqueries';
+
+const MyQueries = () => {
+  const settings = useSelector((state) => state.settings);
+  const { language } = settings;
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.user.userLogin)
+  const { userInformation: userInfo } = userLogin
+  const userDetails = useSelector((state) => state.user.userDetails);
+  const { loading, error, user } = userDetails;
+  const history = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    !userInfo && history('/')
+    dispatch(getUserDetails(id))
+  }, [userInfo, history])
+  return (
+    <div className='myOrders-outermost'>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className='myOrders-inner'>
+          <span className='my-text'>{mq.my[language]}</span>
+          {console.log(user)}
+          {user.queries && user.queries.length < 1 && (
+            <Message
+              message={mq.y[language]}
+              color='black'
+            />
+          )}
+          {user.queries &&
+            user.queries.map((query) => (
+              <div key={query.query} className='orderset'>
+                <div className='orderManage'>
+                  <span>{mq.q[language]} = {query.type}</span>
+                  <span style={{ padding: 0 }}>
+                  {mq.qu[language]} = {query.query}
+                  </span>
+                  <span>
+                    {mq.qs[language]} :
+                    {query.active == true ? (
+                      <span> {mq.a[language]} <i className='fas fa-check'></i></span>
+                    ) : (
+                      <span> {mq.c[language]} <i className='far fa-times-circle'></i></span>
+                    )}
+                  </span>
+                        <div
+                          style={{ marginLeft: '10px', width: '100%' }}
+                          className='underline'
+                        ></div>
+                        </div>
+                        </div>
+                ))}
+                </div>
+            )}
+        </div>
+  )
+}
+
+export default MyQueries
